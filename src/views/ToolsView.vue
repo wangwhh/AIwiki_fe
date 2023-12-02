462fc4924be802fd8252e60cbc5a5376b7e711a6
yuchatto <152359574+yuchatto@users.noreply.github.com>
2023-12-02 14:36:33 +0800

    修改Tools.vue
    初次修改Tools.vue，add ToolsEntries.vue and ToolsMenu.vue

--- src/views/ToolsView.vue
+++ src/views/ToolsView.vue
@@ -1,13 +1,79 @@
 <template>
+    <a-layout>
+        <a-layout-sider class="sider-menu" >
+          <ToolsMenu/>
+        </a-layout-sider>
+        <a-layout>
+            <a-layout-content class="home-content">
+                <ToolsEntries/>
+            </a-layout-content>
+            <a-layout-sider class="right-bar">
+                <a-flex gap="middle" vertical>
+                    <!-- <GoodEntry/> -->
+                    <RecTools/>
+                </a-flex>
+            </a-layout-sider>
+        </a-layout>
 
+    </a-layout>
 </template>
 
 <script>
+import TopBar from "@/components/TopBar.vue";
+import ToolsMenu from "@/components/Tools/ToolsMenu.vue";
+// import GoodEntry from "@/components/Home/GoodEntry.vue";
+import RecTools from "@/components/Home/RecTools.vue";
+import {ref} from "vue";
+import ToolsEntries from "@/components/Tools/ToolsEntries.vue";
+
 export default {
-    name: "ToolsView"
+    name: "ToolsView",
+    components: {ToolsEntries, TopBar, RecTools, ToolsMenu},
+
+    setup() {
+
+    },
+
 }
 </script>
 
 <style scoped>
+.sider-menu {
+    height: 100%;
+    width: 200px;
+    margin-top: 80px;
+    border-radius: 10px;
+    background-color: white;
+    position: fixed;
+
+}
+
+.home-content {
+    background: #fff;
+    /*margin-top: 72px;*/
+    margin: 80px 320px 16px 210px;
+    overflow: initial ;
+    border-radius: 10px;
+    text-align: center;
+    padding: 24px;
+}
+
+.right-bar {
+    min-width: 300px !important;
+    max-width: 300px !important;
+    width: 300px !important;
+    position: fixed;
+    right: 0;
+    margin-right: 10px;
+    margin-top: 80px;
+} 
+
+.ant-layout-header {
+    background: white;
+
+}
+.ant-layout-sider {
+    background: #FFFFFF00;
+} 
 
 </style>
