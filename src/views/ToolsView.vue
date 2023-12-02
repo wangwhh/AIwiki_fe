ade559ddef765b57ab8fdfe33833e8e1123f211e
yuchatto <152359574+yuchatto@users.noreply.github.com>
2023-12-02 15:24:38 +0800

    ToolsView小改动
    路径

--- src/views/ToolsView.vue
+++ src/views/ToolsView.vue
@@ -19,7 +19,7 @@
 </template>
 
 <script>
-import TopBar from "@/components/TopBar.vue";
+import TopBar from "@/components/Menu/TopBar.vue";
 import ToolsMenu from "@/components/Tools/ToolsMenu.vue";
 // import GoodEntry from "@/components/Home/GoodEntry.vue";
 import RecTools from "@/components/Home/RecTools.vue";
