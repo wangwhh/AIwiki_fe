b217ba45d6399f318e1f6555b2063a20e17083f6
wangwhh <2356300214@qq.com>
2023-12-21 22:25:44 +0800

    详情页左侧锚点ok

--- src/views/Entry/components/Anchor.vue
+++ src/views/Entry/components/Anchor.vue
@@ -1,39 +1,64 @@
 <template>
     <a-anchor
-        :target-offset="targetOffset"
         :items=items
-        @click="handleAnchorClick"
+        :offset-top="100"
     ></a-anchor>
 </template>
 
 <script>
-//TODO: highlight the anchor when the corresponding heading is in the viewport
 import {inject, nextTick, onMounted, ref, watch} from 'vue';
+
+
 export default {
     name: 'Anchor',
     setup() {
         let items = ref([]);
-        const targetOffset = ref(undefined);
         const contentLoaded = inject('contentLoaded'); // 从父组件注入
-        function handleAnchorClick(e, link) {
-            e.preventDefault();
-           const heading = document.querySelector(`[data-v-md-line="${link.href}"`);
-           heading.scrollIntoView({
-               behavior: 'smooth',
-               block: 'center',
-           });
-        }
 
         function getTitles() {
             // console.log('getTitles')
             const anchors = ref(document.querySelectorAll('.entry-content h2, .entry-content h3'));
-            // console.log(anchors.value);
+            console.log(anchors.value);
             const titles = Array.from(anchors.value).filter((title) => !!title.innerText.trim());
-            items.value = titles.map((title) => ({
-                key: titles.indexOf(title),
-                href: `${title.attributes[1].nodeValue}`,
-                title: title.innerText,
-            }));
+            console.log(titles)
+            for(let i = 0; i < titles.length; i++) {
+                const level = titles[i];
+                const key = i;
+                const text = level.innerText;
+                const href = level.attributes[1].nodeValue;
+
+                level.setAttribute("id", `${href}`);
+
+                let children=[];
+                if(level.tagName === 'H2') {
+                    let j = i + 1;
+                    for(j = i + 1; j < titles.length; j++) {
+                        let child_level = titles[j];
+                        let child_key = j;
+                        let child_text = child_level.innerText;
+                        let child_href = child_level.attributes[1].nodeValue;
+                        child_level.setAttribute("id", `${child_href}`);
+                        if(titles[j].tagName === 'H3') {
+                            children.push({
+                                key: child_key,
+                                title: child_text,
+                                href: '#' + child_href,
+                            });
+                        } else {
+                            break;
+                        }
+                    }
+                    i = j-1;
+                }
+                const item = {
+                    key: key,
+                    title: text,
+                    href: '#' + href,
+                    children: children,
+                };
+                items.value.push(item);
+                console.log(item)
+            }
         }
 
         watch(contentLoaded, async (newVal) => {
@@ -44,14 +69,10 @@ export default {
             }
         });
         onMounted(() => {
-            window.addEventListener('resize', () => {
-                targetOffset.value = window.innerHeight / 2;
-            });
+
         });
         return {
-            targetOffset,
             items,
-            handleAnchorClick
         };
     },
 };
